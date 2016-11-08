2c66e1fb140076cb201ed5eff0feb3dd67a10ad6
rhobernardi <rodrigo.n.bernardi@gmail.com>
2016-11-08 12:55:44 -0200

    drawing a line and calculate 2 points in this line, 1/3 of the segment

--- HermiteCurve.cpp
+++ HermiteCurve.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int SCREEN_HEIGHT = 500;
-int nPts = 5; // Número de pontos de controle.
+int nPts = 2; // NÃºmero de pontos de controle.
 int N = nPts-1;
 int contPoints = 0;
 
@@ -20,7 +20,7 @@ class Point
 		{
 			x = x2; y = y2;
 		}
-		//Sobrecarga do operador "=" para que não seja atribuído apenas "ponteiros".
+		//Sobrecarga do operador "=" para que nÃ£o seja atribuÃ­do apenas "ponteiros".
 		const Point & operator=(const Point &rPoint)
 		{
 			x = rPoint.x;
@@ -48,6 +48,7 @@ float binomio_coef(int n,int k)
 
 void drawDot(int x, int y)
 {
+	glColor3f(0.0, 0.0, 0.0);
 	glBegin(GL_POINTS);
 		glVertex2i(x,y);
 	glEnd();
@@ -65,8 +66,46 @@ void drawLine(Point p1, Point p2)
 	glFlush();
 }
 
+void calculateVelLine(Point p1, Point p2)
+{
+	int dx = (int)abs(p1.x-p2.x);
+	int dy = (int)abs(p1.y-p2.y);
 
-Point drawBezierGeneralized(Point PT[], double u)
+	int vx1, vx2, vy1, vy2;
+
+	if (p1.x < p2.x) {
+		vx1 = (int)p1.x + (dx/3);
+		vx2 = (int)p2.x - (dx/3);
+	} else {
+		vx1 = (int)p1.x - (dx/3);
+		vx2 = (int)p2.x + (dx/3);
+	}
+
+	if (p1.y < p2.y) {
+		vy1 = (int)p1.y + (dy/3);
+		vy2 = (int)p2.y - (dy/3);
+	} else {
+		vy1 = (int)p1.y - (dy/3);
+		vy2 = (int)p2.y + (dy/3);
+	}
+
+	// cout << "p1.x: " << p1.x << "\tp2.x: " << p2.x << endl;
+	// cout << "p1.y: " << p1.y << "\tp2.y: " << p2.y << "\n" << endl;
+
+	// cout << "dx: " << dx/3 << endl;
+	// cout << "dy: " << dy/3 << "\n" << endl;
+
+	// cout << "v1.x: " << vx1 << "\tv2.x: " << vx2 << endl;
+	// cout << "v1.y: " << vy1 << "\tv2.y: " << vy2 << "\n" << endl;
+
+	PtsDeControle[3].setxy((float) vx1, (float) vy1);
+	PtsDeControle[4].setxy((float) vx2, (float) vy2);
+
+	drawDot(PtsDeControle[3].x, PtsDeControle[3].y);
+	drawDot(PtsDeControle[4].x, PtsDeControle[4].y);
+}
+
+Point drawHermiteCurve(Point PT[], double u)
 {
 	Point P;
 	P.x = 0; P.y = 0;
@@ -83,36 +122,41 @@ void myMouse(int button, int state, int x, int y)
 {
 	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) 
 	{
-		PtsDeControle[contPoints].setxy((float) x, (float) (SCREEN_HEIGHT - y));
-		drawDot(x, SCREEN_HEIGHT - y);
-
-		if (contPoints == nPts)
+		if (contPoints < 2)
 		{
-			glColor3f(0.2, 1.0, 0.0);
+			PtsDeControle[contPoints].setxy((float) x, (float) (SCREEN_HEIGHT - y));
+			drawDot(x, SCREEN_HEIGHT - y);
+			contPoints++;
 
-			for (int i = 0; i < nPts; ++i)
+			if (contPoints == nPts)
 			{
-				drawLine(PtsDeControle[i], PtsDeControle[i+1]);
+				glColor3f(0.2, 1.0, 0.0);
+
+				// for (int i = 0; i < N; ++i)
+				// {
+				// 	drawLine(PtsDeControle[i], PtsDeControle[i+1]);
+				// }
+				
+				drawLine(PtsDeControle[0], PtsDeControle[1]);
+				calculateVelLine(PtsDeControle[0], PtsDeControle[1]);
+
+				glColor3f(0.0, 0.0, 1.0);
+				Point Paux1 = PtsDeControle[0];
+
+				for (double u = 0; u <= 1.0; u+=0.01)
+				{
+					//Point Paux2 = drawHermiteCurve(PtsDeControle, u);
+					//drawLine(Paux1, Paux2);
+
+					//Paux1 = Paux2;
+				}
 			}
-
-			glColor3f(0.0, 0.0, 1.0);
-			Point Paux1 = PtsDeControle[0];
-
-			for (double u = 0; u <= 1.0; u+=0.01)
-			{
-				Point Paux2 = drawBezierGeneralized(PtsDeControle, u);
-				drawLine(Paux1, Paux2);
-
-				Paux1 = Paux2;
-			}
-
-			//contPoints = 0;
 		}
 	}
 }
 
 
-// Funções de cabeçalho.
+// FunÃ§Ãµes de cabeÃ§alho.
 void myInit()
 {
 	glClearColor(1.0,1.0,1.0,0.0);
@@ -121,7 +165,6 @@ void myInit()
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0.0,640.0,0.0,500.0);
-
 }
 
 void myDisplay()
@@ -136,7 +179,7 @@ int main(int argc, char *argv[])
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 	glutInitWindowSize(640,500);
 	glutInitWindowPosition(100,150);
-	glutCreateWindow("Bezier Curve");
+	glutCreateWindow("Hermite Curve");
 	glutMouseFunc(myMouse);
 	glutDisplayFunc(myDisplay);
 	myInit();
